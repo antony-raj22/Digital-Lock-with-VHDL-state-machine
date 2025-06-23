4b4b6e592a52839640efc88e7f0773aba11fc30a
antony <123041179+antony-raj22@users.noreply.github.com>
2025-06-23 21:10:12 +0530

    Update testbench .vhd

--- Digital-Lock-with-VHDL-state-machine-main/src/testbench .vhd
+++ Digital-Lock-with-VHDL-state-machine-main/src/testbench .vhd
@@ -1,30 +1,4 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   17:38:13 12/12/2020
--- Design Name:   
--- Module Name:   C:/Users/AMINE/Desktop/iia4/xilinx/VLSITP/testing.vhd
--- Project Name:  VLSITP
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: SerrureTP4
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 ENTITY testing IS
